6d11cb93ed2f81c8c6a25437478973bf22281261
librarywon <librarywon@naver.com>
2023-10-08 23:34:38 +0900

    [add]: init activity files add

---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/signup/SignupActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.dosopttemplate.presentation.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.dosopttemplate.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
