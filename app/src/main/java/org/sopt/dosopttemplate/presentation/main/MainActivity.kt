6a2b26c14756c3e2c55bcbb38429ebdcea792588
librarywon <librarywon@naver.com>
2023-10-12 03:26:16 +0900

    [feat]: main done

--- app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
@@ -1,12 +1,36 @@
 package org.sopt.dosopttemplate.presentation.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import coil.load
 import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.data.model.User
+import org.sopt.dosopttemplate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val viewModel = MainViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        getUserInfo()
+        initUI()
+    }
+
+    private fun getUserInfo() {
+        val user: User? = intent.getParcelableExtra("user_data") // User 객체 받기
+        if (user != null) {
+            viewModel.setUserInfo(user) // ViewModel에 User 객체 적용
+        }
+    }
+
+    private fun initUI() {
+        with(binding) {
+            ivMainProfile.load(R.drawable.ic_profile)
+            tvMainNickname.text = viewModel.userInfo.value?.nickname
+            tvMainIdValue.text = viewModel.userInfo.value?.id
+            tvMainHobbyValue.text = viewModel.userInfo.value?.hobby
+        }
     }
 }
