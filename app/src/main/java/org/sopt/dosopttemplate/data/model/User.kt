0fba6ab8bb57982f20a96df6909dde2196ee31a7
librarywon <librarywon@naver.com>
2023-10-12 01:46:08 +0900

    [feat]: Serializable user data class

--- app/src/main/java/org/sopt/dosopttemplate/data/model/User.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/model/User.kt
@@ -1,8 +1,10 @@
 package org.sopt.dosopttemplate.data.model
 
+import java.io.Serializable
+
 data class User(
     val id: String,
     val pw: String,
-    val nickname: String?,
-    val hobby: String?
-)
+    val nickname: String,
+    val hobby: String
+) : Serializable
