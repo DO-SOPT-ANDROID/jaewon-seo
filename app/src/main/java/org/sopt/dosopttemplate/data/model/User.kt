72632fbb038828b003546c71e3b58d01d49ff6eb
librarywon <librarywon@naver.com>
2023-10-12 01:53:02 +0900

    [feat]: ktx plugin add for Parcelize

--- app/src/main/java/org/sopt/dosopttemplate/data/model/User.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/model/User.kt
@@ -1,10 +1,12 @@
 package org.sopt.dosopttemplate.data.model
 
-import java.io.Serializable
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class User(
     val id: String,
     val pw: String,
     val nickname: String,
     val hobby: String
-) : Serializable
+) : Parcelable
