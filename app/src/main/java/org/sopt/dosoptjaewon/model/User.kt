35ade07f402515815702bafa2338507f064a9695
librarywon <librarywon@naver.com>
2023-10-27 02:46:19 +0900

    [feat]: remove profileImage

--- app/src/main/java/org/sopt/dosoptjaewon/model/User.kt
+++ app/src/main/java/org/sopt/dosoptjaewon/model/User.kt
@@ -1,13 +1,10 @@
 package org.sopt.dosoptjaewon.data.model
 
 import android.os.Parcelable
-import androidx.annotation.DrawableRes
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class User(
-    @DrawableRes
-    val profileImage: Int,
     val id: String,
     val pw: String,
     val nickname: String,
