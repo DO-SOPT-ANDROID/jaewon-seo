7e068b90d849fda40058f905016d451275d9db0e
librarywon <librarywon@naver.com>
2023-10-27 01:52:47 +0900

    [feat]: edit brithDay class Date to LocateDate

--- app/src/main/java/org/sopt/dosoptjaewon/model/Friend.kt
+++ app/src/main/java/org/sopt/dosoptjaewon/model/Friend.kt
@@ -1,13 +1,13 @@
 package org.sopt.dosoptjaewon.model
 
 import androidx.annotation.DrawableRes
-import java.util.Date
+import java.time.LocalDate
 
 data class Friend(
     @DrawableRes
     val profileImage: Int,
     val name: String,
-    val birthDay: Date,
-    val music: String,
+    val birthDay: LocalDate,
+    val music: String = "",
     val update: Boolean = false
 )
