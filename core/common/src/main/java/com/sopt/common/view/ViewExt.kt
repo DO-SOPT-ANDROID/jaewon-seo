0fc4c0098003e78b41616f35f5759745c015853e
librarywon <librarywon@naver.com>
2023-11-09 16:03:23 +0900

    [refactor]: View.snackBar edit logic

--- core/common/src/main/java/com/sopt/common/view/ViewExt.kt
+++ core/common/src/main/java/com/sopt/common/view/ViewExt.kt
@@ -4,8 +4,8 @@ import android.view.View
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 
-fun View.snackBar(message: String) {
-    Snackbar.make(this, message, Snackbar.LENGTH_SHORT).show()
+fun View.snackBar(messageResId: Int) {
+    Snackbar.make(this, context.getString(messageResId), Snackbar.LENGTH_SHORT).show()
 }
 
 fun View.toast(message: String) {
