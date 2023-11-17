c74b2a4052c6f6ca9105806e6ace1529ca1567c5
librarywon <librarywon@naver.com>
2023-11-17 14:11:27 +0900

    [feat]: remove hobby

--- app/src/main/java/org/sopt/dosoptjaewon/data/network/repository/signup/SignupRepository.kt
+++ app/src/main/java/org/sopt/dosoptjaewon/data/network/repository/signup/SignupRepository.kt
@@ -4,7 +4,6 @@ import org.sopt.dosoptjaewon.data.network.model.signup.SignupRequest
 import org.sopt.dosoptjaewon.data.network.service.AuthService
 import retrofit2.Response
 
-
 class SignupRepository(private val authService: AuthService) {
     suspend fun signup(request: SignupRequest): Response<Unit> {
         return authService.signup(request)
