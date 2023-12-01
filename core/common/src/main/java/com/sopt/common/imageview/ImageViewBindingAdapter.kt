74a3870a7e628e298ad59550c86188dabdda0e21
librarywon <librarywon@naver.com>
2023-12-01 19:08:33 +0900

    [feat]: add ImageViewBindingAdapter

--- core/common/src/main/java/com/sopt/common/imageview/ImageViewBindingAdapter.kt
+++ core/common/src/main/java/com/sopt/common/imageview/ImageViewBindingAdapter.kt
@@ -7,8 +7,17 @@ import coil.transform.CircleCropTransformation
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageCircleUrl(imageUrl: String?) {
-    if (imageUrl != null) {
-        load(imageUrl) {
+    imageUrl?.let {
+        load(it) {
+            transformations(CircleCropTransformation())
+        }
+    }
+}
+
+@BindingAdapter("imageResource")
+fun ImageView.setImageCircleResource(imageResource: Int) {
+    if (imageResource != 0) {
+        load(imageResource) {
             transformations(CircleCropTransformation())
         }
     }
