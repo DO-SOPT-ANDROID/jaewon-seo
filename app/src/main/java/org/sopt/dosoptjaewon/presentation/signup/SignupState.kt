98557fec526ed0bd4bfea4a54d780ba2e4624e58
librarywon <librarywon@naver.com>
2023-12-22 16:27:24 +0900

    [chore]: remove useless import

--- app/src/main/java/org/sopt/dosoptjaewon/presentation/signup/SignupState.kt
+++ app/src/main/java/org/sopt/dosoptjaewon/presentation/signup/SignupState.kt
@@ -1,7 +1,5 @@
 package org.sopt.dosoptjaewon.presentation.signup
 
-import org.sopt.dosoptjaewon.data.model.User
-
 sealed class SignupState {
     data class Success(val userId: String) : SignupState()
     data class Failure(val message: String) : SignupState()
